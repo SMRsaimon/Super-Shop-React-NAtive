d2db6c222a2a92048da1d6395b585621c1bcf534
SMRsaimon <smrsaimon960@gmail.com>
2021-06-02 23:42:58 -0700

    home page change

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Home from './components/Home/Home';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
+import Shop from './components/Shop/Shop';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home/>
+    <SafeAreaView> 
+    <View >
+     <Shop></Shop>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
